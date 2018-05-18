b575df65e69b4671a2f563d9a5fe2e5f894cf495
spongebobmay <spongsbob@163.com>
2018-05-18 10:49:50 +0800

    Add files via upload

---
+++ manacher.py
@@ -0,0 +1,26 @@
+def longestPalindrome(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    slist='#'.join(s)
+    slist= '#'+slist+'#'
+    index=-1
+    pR=-1
+    maxR=0
+    pArr=[0 for i in xrange(len(slist))]
+    for i in xrange(len(slist)):
+        pArr[i]=min(pArr[2*index-i],pR-i) if pR>i else  1
+        while i+pArr[i]<len(slist) and i-pArr[i]>-1:
+            if slist[i+pArr[i]]==slist[i-pArr[i]]:
+                pArr[i]+=1
+            else:
+                break
+        if i+pArr[i]>pR:
+            pR= i+pArr[i]
+            index=i;
+        maxR = max(pArr[i],maxR)
+    return maxR-1
+print longestPalindrome("aabbac")
+
+
